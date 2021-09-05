e6eefbbec3854295edbb37bd1134e49409952d3b
João Marcos <joaodias0078@gmail.com>
2021-09-05 16:26:17 -0400

    Initial commit

---
+++ jsmanual/index.js
@@ -0,0 +1,10 @@
+const typed = new Typed('.type', {
+      strings: [
+          "Estudante",
+          "Desenvolvedor Front-End"
+      ],
+      loop: true,
+      typeSpeed: 80,
+      backSpeed: 50,
+      backDelay: 2000
+    });
